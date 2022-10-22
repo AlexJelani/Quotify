4ccf2b2794729f3fe0c789af4592665bbdb28208
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2022-10-22 14:14:09 +0900

    track styling

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -8,10 +8,16 @@
 			$this->con = $con;
 			$this->id = $id;
 		}
-        public function getName() {
-            $artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
-            $artist = mysqli_fetch_array($artistQuery);
-            return $artist['name'];
-        }
+		public function getName() {
+			$artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
+			$artist = mysqli_fetch_array($artistQuery);
+		 
+			if (!$artist) {
+				echo "<br>No artist found with the id: '$this->id'<br>";
+				exit();
+			}
+		 
+			return $artist['name'];
+		}
     }
 ?>
