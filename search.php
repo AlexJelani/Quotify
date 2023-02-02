7a8a16762fe01457a9c6d10b987910cb46126d6c
Jelani Alexander <jelanialexander82@gmail.com>
2023-02-02 11:49:27 +0900

    added SearchContent css

--- search.php
+++ search.php
@@ -1,14 +1,31 @@
 <?php
 include("includes/includedFiles.php");
 
-if(isset($_GET['term'])){
+if (isset($_GET['term'])) {
     $term = urldecode($_GET['term']);
-}else {
+} else {
     $term = "";
 }
 ?>
 
 <div class="searchContainer">
     <h4>Search for an artist, album, or a song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term;?>" placeholder="Start typing...">
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value; this.selectionStart = this.selectionEnd = this.value.length;">
 </div>
+
+<script>
+    $(".searchInput").focus();
+
+    $(function() {
+        var timer;
+
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+
+            timer = setTimeout(function() {
+                var val = $(".searchInput").val();
+                openPage("search.php?term=" + val);
+            }, 2000);
+        })
+    })
+</script>
