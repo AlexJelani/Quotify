2566ec5e7b84e2ddb572c55cdc8c233a13bdf12f
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-07-18 16:37:20 +0900

    prevented undefined array key warnings

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -1,6 +1,5 @@
 <?php
 class Song {
-
     private $con;
     private $id;
     private $mysqLiData;
@@ -10,25 +9,32 @@ class Song {
     private $genre;
     private $duration;
     private $path;
-   
-   
-   
 
     public function __construct($con, $id) {
         $this->con = $con;
         $this->id = $id;
 
         $query = mysqli_query($this->con, "SELECT * FROM songs WHERE id='$this->id'");
-        $this->mysqLiData = mysqli_fetch_array($query);
-        $this->title = $this->mysqLiData['title'];
-        $this->artistId= $this->mysqLiData['artistId'];
-        $this->albumId = $this->mysqLiData['albumId'];
-        $this->genre = $this->mysqLiData['genre'];
-        $this->duration = $this->mysqLiData['duration'];
-        $this->path = $this->mysqLiData['path'];
-
-    
+        if ($query && mysqli_num_rows($query) > 0) {
+            $this->mysqLiData = mysqli_fetch_assoc($query);
+            $this->title = $this->mysqLiData['title'];
+            $this->artistId = $this->mysqLiData['artistId'] ?? null;
+            $this->albumId = $this->mysqLiData['albumId'] ?? null;
+            $this->genre = $this->mysqLiData['genre'];
+            $this->duration = $this->mysqLiData['duration'];
+            $this->path = $this->mysqLiData['path'];
+        } else {
+            // Handle the case when the song is not found
+            $this->mysqLiData = null;
+            $this->title = null;
+            $this->artistId = null;
+            $this->albumId = null;
+            $this->genre = null;
+            $this->duration = null;
+            $this->path = null;
+        }
     }
+
 
     public function getTitle() {
         return $this->title;
